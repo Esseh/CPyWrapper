8f275da9ae965fc4184c889179a40a07a96263b5
Esseh <kennethwilleford@gmail.com>
2017-10-10 19:16:39 -0700

    added module and function memoization

--- CPyWrapper.cpp
+++ CPyWrapper.cpp
@@ -2,7 +2,9 @@
 #include <unordered_map>
 #include <Python.h>
 
-
+#define SmartPy(variableName,Object) PyPointer variableName=PyPointer(Object,"variableName")
+#define SmartPyFunc(resultName,functionObject,arguments) PyPointer resultName=PyPointer(PyObject_CallObject(functionObject,arguments),"resultName"); 
+#define SmartPyAttr(resultObject,container,handle)	PyPointer resultObject=PyPointer(PyObject_GetAttrString(container,handle),"resultObject");
 class PyPointer{
 	private:
 		std::string name;
@@ -19,34 +21,35 @@ class PyPointer{
 			Py_XDECREF(physicalObject);
 		};
 };
-#define SmartPy(variableName,Object) PyPointer variableName=PyPointer(Object,"variableName")
-#define SmartPyFunc(resultName,functionObject,arguments) PyPointer resultName=PyPointer(PyObject_CallObject(functionObject,arguments),"resultName"); 
-#define SmartPyAttr(resultObject,container,handle)	PyPointer resultObject=PyPointer(PyObject_GetAttrString(container,handle),"resultObject");
+
+class CPyWrapper{
+	public: 
+		static PyObject* GetFunction(std::string moduleName,std::string functionName){
+			static std::unordered_map<std::string,PyObject*> moduleMap = std::unordered_map<std::string,PyObject*>();
+			static std::unordered_map<std::string,PyObject*> functionMap = std::unordered_map<std::string,PyObject*>();
+			if(moduleMap.find(moduleName) == moduleMap.end())
+				moduleMap.insert({moduleName,PyImport_Import(PyString_FromString(moduleName.c_str()))});
+			if(functionMap.find(moduleName+"."+functionName) == functionMap.end())
+				functionMap.insert({
+					moduleName+"."+functionName,
+					PyObject_GetAttrString((moduleMap.find(moduleName))->second,functionName.c_str())
+			});
+			return functionMap.find(moduleName+"."+functionName)->second;
+		}
+};
+
+
 int main(){
 	/// Initialize python interpreter
-    Py_Initialize();
-	/// Grab main: global symbol
-    PyObject* po_main = PyImport_AddModule("__main__");
-	
-	///  Grab module object: Memoize
-    PyObject* wrapper = PyImport_Import(PyString_FromString("CPyWrapper.wrapper"));
-	
-	/// Grab function object: Memoize
-    PyObject* function = PyObject_GetAttrString(wrapper,"executeFunction");	
-	
-	/// Grab function object: Memoize
-    PyObject* higherOrderFunction = PyObject_GetAttrString(wrapper,"tostring");	
-
-	std::cout << function << std::endl;
-	std::cout << higherOrderFunction << std::endl;
+    Py_Initialize();	
 	
     std::string resultString;
     for(int i = 0; i < 1000000; i++){
 		SmartPy(one,PyString_FromString("1.1"));
 		SmartPy(two,PyString_FromString("2.1"));
 		SmartPy(tupleObj,PyTuple_Pack(2,one(),two()));
-		SmartPy(nestedTuple,PyTuple_Pack(2,higherOrderFunction,tupleObj()));
-		SmartPyFunc(functionResult,function,nestedTuple());
+		SmartPy(nestedTuple,PyTuple_Pack(2,CPyWrapper::GetFunction("CPyWrapper.wrapper","tostring"),tupleObj()));
+		SmartPyFunc(functionResult,CPyWrapper::GetFunction("CPyWrapper.wrapper","executeFunction"),nestedTuple());
 		SmartPyAttr(resultObject,functionResult(),"result");
 		resultString = PyString_AsString(resultObject());
     }
