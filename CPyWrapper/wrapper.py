b14dc3cbc1ad9e54837d7854cf7f47f486bc87a4
Esseh <kennethwilleford@gmail.com>
2017-10-10 18:28:46 -0700

    Created smart pointer with macro interface to simplify example

--- CPyWrapper/wrapper.py
+++ CPyWrapper/wrapper.py
@@ -1,6 +1,3 @@
-resultMap = {}
-argMap = {}
-
 class resultObject:
 	def __init__(self,error,result):
 		self.error = error
@@ -9,19 +6,15 @@ class resultObject:
 # Wraps the exceution of a python function automatically catching the error and passing it up in a C++ readable format.
 def executeFunction(function,args):
 	global resultMap
-	global argMap
-	argMap[function.__name__] = args
 	try:
 		# If nothing goes wrong then return no error along with the result of the function
 		result = resultObject("",function(*args))
-		resultMap[function.__name__] = result
 		return result
 	except Exception as e:
 		# Otherwise pass the error up in a format usable by C++.
 		errorType = type(e)
 		truncatedErrorType = str(errorType)[7:-2]
 		result = resultObject(truncatedErrorType, None)
-		resultMap[function.__name__] = result
 		return result
 		
 def tostring(a,b):
